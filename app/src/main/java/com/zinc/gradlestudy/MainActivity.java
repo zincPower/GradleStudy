a3bc15474d5ecd005511f3f065f1fa25524c08e4
zincPower <56002982@qq.com>
2019-10-17 07:28:57 +0800

    1、拆解配置

--- app/src/main/java/com/zinc/gradlestudy/MainActivity.java
+++ app/src/main/java/com/zinc/gradlestudy/MainActivity.java
@@ -3,11 +3,8 @@ package com.zinc.gradlestudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
-import com.zinc.library.Test;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
 //        TestUtils.test();
 
-        Test test = new Test();
-        test.show(this);
-
-        int hi = test.hi(1, 4);
-        ((TextView) findViewById(R.id.tv_content)).setText("江澎涌 zinc " + hi);
-
-        String name = BuildConfig.name;
-        int age = BuildConfig.age;
+//        Test test = new Test();
+//        test.show(this);
+//
+//        int hi = test.hi(1, 4);
+//        ((TextView) findViewById(R.id.tv_content)).setText("江澎涌 zinc " + hi);
+//
+//        String name = BuildConfig.name;
+//        int age = BuildConfig.age;
 
         ZincPower zincPower = new ZincPower();
         int zinc = zincPower.hi(1, 4);

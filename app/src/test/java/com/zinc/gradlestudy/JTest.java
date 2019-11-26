93466521fd22e618bbbe33273905052b71d919a7
zincPower <56002982@qq.com>
2019-11-26 10:16:09 +0800

    1、整理完 lint 2、调整了结构

--- app/src/test/java/com/zinc/gradlestudy/JTest.java
+++ app/src/test/java/com/zinc/gradlestudy/JTest.java
@@ -2,14 +2,14 @@ package com.zinc.gradlestudy;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class JTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
